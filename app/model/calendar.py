a8ca8906dfcae68059ec0091bbb53206fa01684f
valentina sierra <valentina.sierra.ospina1220thc@gmail.com>
2025-04-02 07:46:14 -0500

    implemente la clase Reminder

--- app/model/calendar.py
+++ app/model/calendar.py
@@ -1,18 +1,7 @@
-from dataclasses import dataclass, field
-from datetime import datetime, date, time
-from typing import ClassVar
-
-from app.services.util import generate_unique_id, date_lower_than_today_error, event_not_found_error, \
-    reminder_not_found_error, slot_not_available_error
-
-
-# TODO: Implement Reminder class here
-
-
-# TODO: Implement Event class here
-
-
-# TODO: Implement Day class here
-
-
-# TODO: Implement Calendar class here
+from dataclasses import dataclass
+from datetime import datetime
+@dataclass
+class Reminder:
+    EMAIL = "email"
+    SYSTEM = "system"
+    date_time: datetime
