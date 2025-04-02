02101798cb8c21b7b1e8d5e2ddcb9d6ef8e85643
valentina sierra <valentina.sierra.ospina1220thc@gmail.com>
2025-04-02 18:15:03 -0500

    se creo otra clase como dataclass, en este caso la clase Event

--- app/model/calendar.py
+++ app/model/calendar.py
@@ -10,5 +10,15 @@ class Reminder:
     def __str__(self):
         return f"Reminder on {self.date_time} of type {self.type}"
 
+@dataclass
+class Event:
+    title: str
+    description: str
+    date_: date
+    start_at: time
+    end_at: time
+    reminders: list = field(default_factory=list)
+    id: str = field(default_factory=generate_unique_id)
+
 
 
