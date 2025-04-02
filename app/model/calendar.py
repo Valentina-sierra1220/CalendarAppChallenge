0913610985d0e8da474dc6f19c51ff668a44b545
valentina sierra <valentina.sierra.ospina1220thc@gmail.com>
2025-04-02 18:13:41 -0500

    se finalizo la clase Reminder

--- app/model/calendar.py
+++ app/model/calendar.py
@@ -5,4 +5,10 @@ class Reminder:
     EMAIL = "email"
     SYSTEM = "system"
     date_time: datetime
+    type: str = EMAIL
+
+    def __str__(self):
+        return f"Reminder on {self.date_time} of type {self.type}"
+
+
 
